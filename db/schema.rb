92b20c87524ed7b412a294099bcbb37713f46975
Takeshi Uto <uto2047@gmail.com>
2023-06-13 15:20:22 +0900

    Make a basic User model (with secure passwords)

--- db/schema.rb
+++ db/schema.rb
@@ -14,8 +14,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_29_023929) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
